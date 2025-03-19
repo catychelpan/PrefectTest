5c9fd63e454b4311087a845313fa08eb2b2a6b08
catychelpan <catychelpan@gmail.com>
2025-03-19 03:07:10 +0200

    fixes for dependencies in deployment file

--- create_deployment.py
+++ create_deployment.py
@@ -5,13 +5,13 @@ SOURCE_REPO = "https://github.com/catychelpan/PrefectTest.git"
 
 if __name__ == "__main__":
     flow.from_source(
-        source=SOURCE_REPO,
-        entrypoint="test_script.py:etl_github_data",  
-    ).deploy(
-        name="my-first-deployment",  # Name of the deployment
-        parameters={  
-            "example_param": "value"
-        },
-        work_pool_name="my-work-pool",  # Work pool you created earlier
-        cron="0 * * * *",  # Schedule the flow to run every hour
-    )
+    source=SOURCE_REPO,
+    entrypoint="test_script.py:etl_github_data",
+).deploy(
+    name="my-first-deployment",
+    parameters={"example_param": "value"},
+    work_pool_name="my-work-pool",
+    cron="0 * * * *",
+    infrastructure="process",
+    env={"PREFECT_EXTRAS_REQUIREMENTS": "pandas httpx"},
+)
