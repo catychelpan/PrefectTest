cc4edaea2c8abd1f38e2eebd6348b4f5c6387914
catychelpan <catychelpan@gmail.com>
2025-03-19 03:23:07 +0200

    Rolled back deployement changes

--- create_deployment.py
+++ create_deployment.py
@@ -1,14 +1,18 @@
 from prefect import flow
-from test_script import etl_github_data  # Import the flow from your script
 
-# URL of your GitHub repository (you can skip this if you're using a local script)
+# URL of your repository
 SOURCE_REPO = "https://github.com/catychelpan/PrefectTest.git"
 
 if __name__ == "__main__":
-    etl_github_data.deploy(
+    flow.from_source(
+        source=SOURCE_REPO,
+        entrypoint="test_script.py:etl_github_data",  
+    ).deploy(
         name="my-first-deployment",  # Name of the deployment
-        work_pool_name="my-work-pool",  # The work pool you created
+        parameters={  
+            "example_param": "value"
+        },
+        work_pool_name="my-work-pool",  # Work pool you created earlier
         cron="0 * * * *",  # Schedule the flow to run every hour
-        # Remove 'env' here as it's no longer used
-        extra_requirements=["pandas", "httpx"],  # Ensure dependencies are installed in the environment
+    
     )
