adfe963976cae02a27dee28cd0d8d2cc6c57f305
catychelpan <catychelpan@gmail.com>
2025-03-19 02:39:06 +0200

    Updated deployment configurations

--- create_deployment.py
+++ create_deployment.py
@@ -6,10 +6,10 @@ SOURCE_REPO = "https://github.com/catychelpan/PrefectTest.git"
 if __name__ == "__main__":
     flow.from_source(
         source=SOURCE_REPO,
-        entrypoint="your_flow_script.py:your_flow_function",  # Replace with your flow file and function name
+        entrypoint="test_script.py:etl_github_data",  
     ).deploy(
         name="my-first-deployment",  # Name of the deployment
-        parameters={  # Add any parameters that your flow might need
+        parameters={  
             "example_param": "value"
         },
         work_pool_name="my-work-pool",  # Work pool you created earlier
